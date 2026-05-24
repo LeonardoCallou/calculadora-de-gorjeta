5f8525f04ef9104c3cd148b6b39be88997ee8f42
Leonardo Callou <lelecocalloucerqueira@gmail.com>
2026-05-23 22:22:11 -0300

    correção de pequenos erros

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ function limpar() {
 
     pessoasInput.value = ""
 
-    document.querySelector(".gorgeta-total > strong").innerHTML = "R$ 0.00"
+    document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
     document.querySelector(".total > strong").innerHTML = "R$ 0.00"
 
     conta = 0
